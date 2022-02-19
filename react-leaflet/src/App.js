70f0039f377c216fe3fb6c76af4b32313822f585
unknown <tsertic5@gmail.com>
2022-02-19 16:51:59 +0100

    finished simple react-leaflet demo to set marker on map and get its position

--- react-leaflet/src/App.js
+++ react-leaflet/src/App.js
@@ -1,7 +1,45 @@
 import "./App.css";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMapEvents,
+} from "react-leaflet";
+import { useState } from "react";
 
+/* set marker on map and get its lat and lon */
 function App() {
-  return <div className="App"></div>;
+  return (
+    <MapContainer center={{ lat: 51.505, lng: -0.09 }} zoom={13}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <LocationMarker />
+    </MapContainer>
+  );
+}
+
+function LocationMarker() {
+  const [marker, setMarkers] = useState(null);
+  const map = useMapEvents({
+    click(e) {
+      const newMarker = e.latlng;
+      console.log(newMarker);
+      setMarkers(() => newMarker);
+    },
+  });
+
+  return (
+    <>
+      {marker && (
+        <Marker position={marker}>
+          <Popup>Marker is at {marker.lat}</Popup>
+        </Marker>
+      )}
+    </>
+  );
 }
 
 export default App;
